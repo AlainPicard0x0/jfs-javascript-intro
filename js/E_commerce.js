41992df3997d0c657c4c820f03c5e91e3b146fa0
eaglepython <josephbidias@yahoo.com>
2022-01-14 14:18:05 -0600

    Final edit !

--- js/E_commerce.js
+++ js/E_commerce.js
@@ -1,3 +1,16 @@
+/* E-Commerce Item list
+
+Use a swtich statement to print the price of the each item in the store to the console.
+Ex. If the value of the variable is 'shoes' then, the statement Shoes are $50 should be printed to the console.
+Reference: Codecademy lesson 10
+Items:
+
+Shoes- $50
+Jeans- $25
+Hat- $12
+Socks- $2
+If the variable input is not an item in the store, then print 'Invalid Item' to the console. */
+
 let item = 'shoes';
 switch (item) {
     case 'shoes':
